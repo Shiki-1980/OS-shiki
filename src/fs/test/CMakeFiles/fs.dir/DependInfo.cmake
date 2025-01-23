05d8f24c16c8ab2c0c3e1d38f525dc4e74a912ca
shiki <119944743+Shiki-1980@users.noreply.github.com>
2025-01-23 14:09:43 +0800

    Add files via upload

---
+++ src/fs/test/CMakeFiles/fs.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/data/OS-24FALL-FDU/src/fs/block_device.c" "CMakeFiles/fs.dir/block_device.o" "gcc" "CMakeFiles/fs.dir/block_device.o.d"
+  "/root/data/OS-24FALL-FDU/src/fs/cache.c" "CMakeFiles/fs.dir/cache.o" "gcc" "CMakeFiles/fs.dir/cache.o.d"
+  "/root/data/OS-24FALL-FDU/src/fs/file.c" "CMakeFiles/fs.dir/file.o" "gcc" "CMakeFiles/fs.dir/file.o.d"
+  "/root/data/OS-24FALL-FDU/src/fs/fs.c" "CMakeFiles/fs.dir/fs.o" "gcc" "CMakeFiles/fs.dir/fs.o.d"
+  "/root/data/OS-24FALL-FDU/src/fs/inode.c" "CMakeFiles/fs.dir/inode.o" "gcc" "CMakeFiles/fs.dir/inode.o.d"
+  "/root/data/OS-24FALL-FDU/src/fs/pipe.c" "CMakeFiles/fs.dir/pipe.o" "gcc" "CMakeFiles/fs.dir/pipe.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/fs/test/CMakeFiles/fs.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fs.dir/block_device.o"
+  "CMakeFiles/fs.dir/block_device.o.d"
+  "CMakeFiles/fs.dir/cache.o"
+  "CMakeFiles/fs.dir/cache.o.d"
+  "CMakeFiles/fs.dir/file.o"
+  "CMakeFiles/fs.dir/file.o.d"
+  "CMakeFiles/fs.dir/fs.o"
+  "CMakeFiles/fs.dir/fs.o.d"
+  "CMakeFiles/fs.dir/inode.o"
+  "CMakeFiles/fs.dir/inode.o.d"
+  "CMakeFiles/fs.dir/pipe.o"
+  "CMakeFiles/fs.dir/pipe.o.d"
+  "libfs.a"
+  "libfs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
